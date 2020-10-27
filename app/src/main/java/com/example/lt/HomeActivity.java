e1b90c37499272561a5dc59fe93f4379d33de781
abdul <abdul.tanveer@gmail.com>
2020-10-27 16:12:39 +0530

    logs and logcat

--- app/src/main/java/com/example/lt/HomeActivity.java
+++ app/src/main/java/com/example/lt/HomeActivity.java
@@ -3,14 +3,16 @@ package com.example.lt;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
-
+    public  static String TAG = HomeActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        Log.d(TAG,"created");
         String data = getIntent().getExtras().getString("mykey");
         TextView greetingTextView =  findViewById(R.id.textView_greeting);
         greetingTextView.setText(data);
--- app/src/main/java/com/example/lt/MainActivity.java
+++ app/src/main/java/com/example/lt/MainActivity.java
@@ -4,30 +4,39 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     EditText nameEditText;    //declaration
+    public static String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i(TAG,"activity created");
         nameEditText = findViewById(R.id.editTextTextPersonName);  //init
     }
 
     public void clickHandler(View view) {
+        Log.v(TAG,"button clicked");
+
         switch (view.getId()){
             case R.id.buttonlogin:
+                Log.w(TAG,"logging in");
+
                 String name = nameEditText.getText().toString(); //using
                // Toast.makeText(this, "welcome to android "+name, Toast.LENGTH_SHORT).show();
                 Intent homeIntent = new Intent(MainActivity.this,HomeActivity.class);
-                homeIntent.putExtra("mykey",name);
+               homeIntent.putExtra("mykey",name);
                 startActivity(homeIntent);
                 break;
             case R.id.buttoncancel:
+                Log.e(TAG,"cancelling");
+
                 Toast.makeText(this, "cancelled ", Toast.LENGTH_SHORT).show();
                 break;
         }
