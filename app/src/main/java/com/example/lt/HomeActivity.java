ed0f23ccdd2971dab0ae4d5ee1e6c8bca3716816
abdul <abdul.tanveer@gmail.com>
2020-10-27 15:26:38 +0530

    start another activity and send data

---
+++ app/src/main/java/com/example/lt/HomeActivity.java
@@ -0,0 +1,18 @@
+package com.example.lt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        String data = getIntent().getExtras().getString("mykey");
+        TextView greetingTextView =  findViewById(R.id.textView_greeting);
+        greetingTextView.setText(data);
+    }
+}
--- app/src/main/java/com/example/lt/MainActivity.java
+++ app/src/main/java/com/example/lt/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.lt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -18,7 +19,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void clickHandler(View view) {
-        String name = nameEditText.getText().toString(); //using
-        Toast.makeText(this, "welcome to android "+name, Toast.LENGTH_SHORT).show();
-    }
+        switch (view.getId()){
+            case R.id.buttonlogin:
+                String name = nameEditText.getText().toString(); //using
+               // Toast.makeText(this, "welcome to android "+name, Toast.LENGTH_SHORT).show();
+                Intent homeIntent = new Intent(MainActivity.this,HomeActivity.class);
+                homeIntent.putExtra("mykey",name);
+                startActivity(homeIntent);
+                break;
+            case R.id.buttoncancel:
+                Toast.makeText(this, "cancelled ", Toast.LENGTH_SHORT).show();
+                break;
+        }
+       }
+
+       private  int addTwoNums(int firstno, int secondNo){
+        return firstno+secondNo;
+       }
 }
