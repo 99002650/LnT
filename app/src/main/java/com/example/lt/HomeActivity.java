03c8fad00e5eb4b1649a3ec15d94b0bed45e9a23
abdul <abdul.tanveer@gmail.com>
2020-10-28 11:43:05 +0530

    listview and adapters

--- app/src/main/java/com/example/lt/HomeActivity.java
+++ app/src/main/java/com/example/lt/HomeActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
@@ -15,9 +17,19 @@ public class HomeActivity extends AppCompatActivity {
         Log.d(TAG,"created");
         int c = subTwoNos(30,20);
 
-        String data = getIntent().getExtras().getString("mykey");
+        /*String data = getIntent().getExtras().getString("mykey");
         TextView greetingTextView =  findViewById(R.id.textView_greeting);
-        greetingTextView.setText(data);
+        greetingTextView.setText(data);*/
+
+        ListView languagesListView = findViewById(R.id.languages_listview);
+        String[] languages = new String[]{"english","hindi","urdu","kannada"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+               // android.R.layout.simple_list_item_1,//layout of each row in the listview
+                R.layout.row_layout_listview,
+                R.id.textViewTitle,
+                languages);
+        languagesListView.setAdapter(adapter);
     }
 
     private int subTwoNos(int i, int i1) {
