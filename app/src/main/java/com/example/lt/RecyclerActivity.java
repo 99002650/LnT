8479cea221990f7e0a0234abf574f06f88aedf85
abdul <abdul.tanveer@gmail.com>
2020-10-28 12:12:04 +0530

    Recyclerview part1

---
+++ app/src/main/java/com/example/lt/RecyclerActivity.java
@@ -0,0 +1,33 @@
+package com.example.lt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class RecyclerActivity extends AppCompatActivity {
+
+    ArrayList<Note> noteArrayList;//declaration
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycler);
+        noteArrayList = new ArrayList<>(); //initialization
+
+        createData();
+    }
+
+    private void createData() {
+        noteArrayList.add(new Note("title1","subtitle1"));
+        noteArrayList.add(new Note("title2","subtitle1"));
+        noteArrayList.add(new Note("title13","subtitle1"));
+        noteArrayList.add(new Note("title14","subtitle1"));
+        noteArrayList.add(new Note("title15","subtitle1"));
+        noteArrayList.add(new Note("title16","subtitle1"));
+        noteArrayList.add(new Note("title17","subtitle1"));
+        noteArrayList.add(new Note("title18","subtitle1"));
+
+    }
+}
