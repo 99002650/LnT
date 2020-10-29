153bf4cd98fcb85e9636bb7b9f3b2acd504ef869
abdul <abdul.tanveer@gmail.com>
2020-10-29 14:41:58 +0530

    connect to internet part3

--- app/src/main/java/com/example/lt/FetchBook.java
+++ app/src/main/java/com/example/lt/FetchBook.java
@@ -4,6 +4,9 @@ import android.os.AsyncTask;
 import android.util.Log;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 public class FetchBook  extends AsyncTask<String,Void,String> {
     private static final String TAG = FetchBook.class.getSimpleName();
     private TextView mTitleText;
@@ -25,6 +28,33 @@ public class FetchBook  extends AsyncTask<String,Void,String> {
     protected void onPostExecute(String jsonString) {
         super.onPostExecute(jsonString);
         Log.d(TAG, "json string = "+jsonString);
+
+        try{
+            JSONObject jsonObject = new JSONObject(jsonString);
+            JSONArray itemsArray = jsonObject.getJSONArray("items");
+
+            for(int i = 0; i<itemsArray.length(); i++){
+                JSONObject book = itemsArray.getJSONObject(i); //Get the current item
+                String title=null;
+                String authors=null;
+                JSONObject volumeInfo = book.getJSONObject("volumeInfo");
+
+                try {
+                    title = volumeInfo.getString("title");
+                    authors = volumeInfo.getString("authors");
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+                //If both a title and author exist, update the TextViews and return
+                if (title != null && authors != null){
+                    mTitleText.setText(title);
+                    mAuthorText.setText(authors);
+                    return;
+                }
+
+            }
+        }catch (Exception e){}
+
 
     }
 }
