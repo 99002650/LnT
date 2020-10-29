2e1719ee8fc97281f6520586d5ff77f200537985
abdul <abdul.tanveer@gmail.com>
2020-10-29 11:47:53 +0530

    async task part-2

--- app/src/main/java/com/example/lt/DownloadActivity.java
+++ app/src/main/java/com/example/lt/DownloadActivity.java
@@ -5,20 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ProgressBar;
 
 public class DownloadActivity extends AppCompatActivity {
     public static String TAG = MainActivity.class.getSimpleName();
+    ProgressBar mProgressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_download);
         Log.i(TAG,"activity created");
+        mProgressBar = findViewById(R.id.progressBar);
     }
 
     public void downloadHandler(View view) {
         Log.i(TAG,"button clicked");
 
-        DownloadTask downloadTask = new DownloadTask();
+        DownloadTask downloadTask = new DownloadTask(mProgressBar);
         downloadTask.execute("http://lnt.com/imagetobedownloaded");
 
     }
--- app/src/main/java/com/example/lt/DownloadTask.java
+++ app/src/main/java/com/example/lt/DownloadTask.java
@@ -2,14 +2,26 @@ package com.example.lt;
 
 import android.os.AsyncTask;
 import android.util.Log;
+import android.widget.ProgressBar;
 
 public class DownloadTask extends AsyncTask<String,Integer,Void> {
     public static String TAG = DownloadTask.class.getSimpleName();
+    ProgressBar progressBar;
+
+    public DownloadTask(ProgressBar mProgressBar) {
+        progressBar = mProgressBar;
+    }
 
     @Override
     protected Void doInBackground(String... downloadUrl) {
         Log.i(TAG,"doInBackground --downloadUrl= "+downloadUrl[0]);
-
+        publishProgress(50);
         return null;
+    }
+
+    @Override
+    protected void onProgressUpdate(Integer... values) {
+        super.onProgressUpdate(values);
+        progressBar.setProgress(values[0]);
     }
 }
