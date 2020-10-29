24718dc923d6a75445f9b1eb738067e7175690db
abdul <abdul.tanveer@gmail.com>
2020-10-29 11:57:36 +0530

    async task part-3 onpreexecute and postexecute

--- app/src/main/java/com/example/lt/DownloadTask.java
+++ app/src/main/java/com/example/lt/DownloadTask.java
@@ -2,6 +2,7 @@ package com.example.lt;
 
 import android.os.AsyncTask;
 import android.util.Log;
+import android.view.View;
 import android.widget.ProgressBar;
 
 public class DownloadTask extends AsyncTask<String,Integer,Void> {
@@ -13,15 +14,39 @@ public class DownloadTask extends AsyncTask<String,Integer,Void> {
     }
 
     @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        progressBar.setVisibility(View.VISIBLE);
+    }
+
+    @Override
     protected Void doInBackground(String... downloadUrl) {
         Log.i(TAG,"doInBackground --downloadUrl= "+downloadUrl[0]);
-        publishProgress(50);
+        try {
+        for (int i=1; i<21;i++){
+            publishProgress(i*5);
+
+                Thread.sleep(100);
+            }
+
+        }
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
     @Override
     protected void onProgressUpdate(Integer... values) {
         super.onProgressUpdate(values);
+        Log.i(TAG,"onProgressUpdate --progress= "+values[0]);
+
         progressBar.setProgress(values[0]);
+    }
+
+    @Override
+    protected void onPostExecute(Void aVoid) {
+        super.onPostExecute(aVoid);
+        progressBar.setVisibility(View.INVISIBLE);
     }
 }
