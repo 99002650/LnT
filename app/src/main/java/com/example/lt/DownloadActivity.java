3542fe392dee0b076a3e5598218307ab9d5caf57
abdul <abdul.tanveer@gmail.com>
2020-10-29 13:59:44 +0530

    connect to internet part1

--- app/src/main/java/com/example/lt/DownloadActivity.java
+++ app/src/main/java/com/example/lt/DownloadActivity.java
@@ -5,24 +5,34 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ProgressBar;
 
 public class DownloadActivity extends AppCompatActivity {
     public static String TAG = MainActivity.class.getSimpleName();
     ProgressBar mProgressBar;
+    EditText mBookInput;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_download);
         Log.i(TAG,"activity created");
+         mBookInput = findViewById(R.id.bookInput);
         mProgressBar = findViewById(R.id.progressBar);
     }
 
     public void downloadHandler(View view) {
         Log.i(TAG,"button clicked");
+        searchBooks();
 
-        DownloadTask downloadTask = new DownloadTask(mProgressBar);
-        downloadTask.execute("http://lnt.com/imagetobedownloaded");
+       /* DownloadTask downloadTask = new DownloadTask(mProgressBar);
+        downloadTask.execute("http://lnt.com/imagetobedownloaded");*/
+
+    }
+
+
+    private void searchBooks() {
+        String queryString = mBookInput.getText().toString();
 
     }
 }
---
+++ app/src/main/java/com/example/lt/FetchBook.java
@@ -0,0 +1,27 @@
+package com.example.lt;
+
+import android.os.AsyncTask;
+import android.widget.TextView;
+
+public class FetchBook  extends AsyncTask<String,Void,String> {
+    private TextView mTitleText;
+    private TextView mAuthorText;
+
+    public FetchBook(TextView mTitleText, TextView mAuthorText) {
+        this.mTitleText = mTitleText;
+        this.mAuthorText = mAuthorText;
+    }
+
+
+
+
+    @Override
+    protected String doInBackground(String... strings) {
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(String jsonString) {
+        super.onPostExecute(jsonString);
+    }
+}
