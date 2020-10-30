da8f3008d21225aceb793794f2ece5403ce9a5bf
abdul <abdul.tanveer@gmail.com>
2020-10-30 10:17:43 +0530

    playing music in service

--- app/src/main/java/com/example/lt/MusicService.java
+++ app/src/main/java/com/example/lt/MusicService.java
@@ -2,10 +2,12 @@ package com.example.lt;
 
 import android.app.Service;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.IBinder;
 import android.widget.Toast;
 
 public class MusicService extends Service {
+    MediaPlayer mediaPlayer;
     public MusicService() {
     }
 
@@ -16,10 +18,18 @@ public class MusicService extends Service {
     }
 
     @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+         super.onStartCommand(intent, flags, startId);
+         mediaPlayer = MediaPlayer.create(this,R.raw.music);
+        mediaPlayer.start();
+        return START_STICKY;
+    }
+
+    @Override
     public void onDestroy() {
         super.onDestroy();
         Toast.makeText(this, "service destroyed", Toast.LENGTH_SHORT).show();
-
+        mediaPlayer.stop();
     }
 
     @Override
